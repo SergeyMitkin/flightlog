4ef710f26558664ddc963e97819f073a849c0089
SergeyMitkin <sergeymitkin@gmail.com>
2020-11-15 04:24:37 +0300

    CRLF для всех файлов

--- controllers/C_Base.php
+++ controllers/C_Base.php
@@ -1,1 +1,27 @@
-<?phpinclude_once('Controller.php');// Базовый контроллер сайтаabstract class C_Base extends Controller{	protected $title;		// заголовок страницы	protected $content;		// содержание страницы	//	// Конструктор.	//	function __construct()	{				$this->title = 'Журнал подготовки к полетам беспилотной авиационной системы';		$this->content = '';	}	// Генерация базового шаблонаы	public function render()	{		$vars = array('title' => $this->title, 'content' => $this->content);			$page = $this->Template('../views/main.php', $vars);		echo $page;	}	}
+<?php
+include_once('Controller.php');
+
+// Базовый контроллер сайта
+
+abstract class C_Base extends Controller
+{
+	protected $title;		// заголовок страницы
+	protected $content;		// содержание страницы
+
+	//
+	// Конструктор.
+	//
+	function __construct()
+	{		
+		$this->title = 'Журнал подготовки к полетам беспилотной авиационной системы';
+		$this->content = '';
+	}
+
+	// Генерация базового шаблонаы
+	public function render()
+	{
+		$vars = array('title' => $this->title, 'content' => $this->content);	
+		$page = $this->Template('../views/main.php', $vars);
+		echo $page;
+	}	
+}
--- controllers/C_Page.php
+++ controllers/C_Page.php
@@ -1,1 +1,28 @@
-<?php// include_once('inc/model.php');include_once('C_Base.php');// Конттроллер страницclass C_Page extends C_Base{	// Конструктор	function __construct(){				parent::__construct();	}		public function action_generalTraining(){		$this->title .= '';		$text = 'general training';		$this->content = $this->Template(VIEW_DIR . '/general_training.php', array('text' => $text));	}    public function action_training(){        $this->title .= '';        $text = 'training';        $this->content = $this->Template(VIEW_DIR . '/training.php', array('text' => $text));    }}
+<?php
+
+// include_once('inc/model.php');
+include_once('C_Base.php');
+
+// Конттроллер страниц
+
+class C_Page extends C_Base
+{
+	// Конструктор
+	function __construct(){		
+		parent::__construct();
+	}
+	
+	public function action_generalTraining(){
+
+		$this->title .= '';
+		$text = 'general training';
+		$this->content = $this->Template(VIEW_DIR . '/general_training.php', array('text' => $text));
+	}
+
+    public function action_training(){
+
+        $this->title .= '';
+        $text = 'training';
+        $this->content = $this->Template(VIEW_DIR . '/training.php', array('text' => $text));
+    }
+}
