a3812cb23df34b5e918e5e8d51ea236ab3bd3867
SergeyMitkin <sergeymitkin@gmail.com>
2020-11-18 00:19:24 +0300

    При загрузке страницы, задачи выводятся по дате

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,37 +1,28 @@
-
-
 var elTaskRow = document.getElementById("row-tasks");
 var elTaskCalendarDiv = document.getElementById("task-calendar-div");
 
+function getTasksByMonth(){
+    var elSelectYear = document.getElementById("year-task-select");
+    var elSelectMonth = document.getElementById("month-task-select");
+    var task_date = elSelectYear.value + '-' + elSelectMonth.value;
+
+    var m, k;
+    m=elTaskRow.querySelectorAll(".general-task-item");
+    k=m.length;
+    while(k--){
+        m[k].setAttribute('hidden', '');
+
+        if (m[k].getAttribute('data-sort-date') == task_date){
+            m[k].removeAttribute('hidden');
+        }
+    }
+}
+
+// При загрузке страницы и при смене select года или месяца, выводим задачи по дате
+document.addEventListener("DOMContentLoaded", getTasksByMonth);
 elTaskCalendarDiv.addEventListener('change', event =>{
     if (event.target.className == 'general-task-select'){
-        var elSelectYear = document.getElementById("year-task-select");
-        var elSelectMonth = document.getElementById("month-task-select");
-        var task_date = elSelectYear.value + '-' + elSelectMonth.value;
-
-
-        var m, k;
-        m=elTaskRow.querySelectorAll(".general-task-item");
-        k=m.length;
-        while(k--){
-            m[k].setAttribute('hidden', '');
-
-            if (m[k].getAttribute('data-sort-date') == task_date){
-                m[k].removeAttribute('hidden');
-            }
-
-            //console.log(m[k].getAttribute('data-sort-date'));
-        }
-//if(m[k].parentNode.parentNode.tagName.search(/div/i)<0){
-// или так
-              //  if(m[k].parentNode.parentNode.tagName!="DIV"){
-                //    m[k].style.display="none";}};
-
-
-
-
-        //console.log(m);
+        getTasksByMonth();
     }
 })
-
 
