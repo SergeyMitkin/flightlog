f3ef85036b5209bf20f71e1a3d9e9386df0ce612
SergeyMitkin <sergeymitkin@gmail.com>
2020-11-29 15:48:29 +0300

    Добавлена кнопка "Редактировать" в карточку полёта

--- public/js/flight_edit.js
+++ public/js/flight_edit.js
@@ -1,8 +1,15 @@
-var elRowFlights = document.getElementById("row-flights");
+var elRowFlights = document.getElementById("row-flights"); // Div со списком полётов
+var elFlightCreateDiv = document.getElementById("flight-create-div"); // Div с формой добавления полёта
 
+// При клике на кнопку "Реактировать", помещаем форму редактирования в карточку полёта
 elRowFlights.addEventListener("click", event =>{
     if (event.target.className == "flight-edit-button"){
-        console.log('edit');
+        var flight_id = event.target.id.split("_")[1];
+        var elFlightItemDiv = document.getElementById("flight-item_" + flight_id);
+        elFlightItemDiv.appendChild(elFlightCreateDiv);
+        elFlightCreateDiv.removeAttribute("hidden");
+
+        console.log(elFlightItemDiv);
     }
 })
 // var elFlightEditButtons = document.querySelectorAll(".flight-edit-button");
