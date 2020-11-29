20798201e2af7fe4168dd57b474a6df817e35e02
SergeyMitkin <sergeymitkin@gmail.com>
2020-11-29 08:51:32 +0300

    Добавлена функции перелистывания календаря на предыдущий день

--- public/js/t_calendar.js
+++ public/js/t_calendar.js
@@ -3,13 +3,9 @@ var elTrainingCalendar = document.getElementById("training-calendar");
 var elFlightButtonBack = document.getElementById("flight-button-back");
 var elFlightButtonForward = document.getElementById("flight-button-forward");
 
-getItemsByDate();
-
 function getDate() {
     var date = elTrainingCalendar.value;
     var elFlightsTitle = document.getElementById("flights-title");
-    console.log(date);
-    console.log(formatDate(date));
     elFlightsTitle.innerText = "Полёты на  " + formatDate(date);
 
     return date;
@@ -29,6 +25,40 @@ function getItemsByDate(classname = "row-item") {
         }
     }
 }
+
+// При загрузке страницы и при смене даты, выводим задачи по дате
+document.addEventListener("DOMContentLoaded", getItemsByDate());
+elTrainingCalendar.addEventListener('change', event => {
+    getItemsByDate();
+})
+
+// Получаем задачи на предыдущий день
+elFlightButtonBack.addEventListener('click', event => {
+    var d = new Date(getDate());
+    d.setDate(d.getDate() - 1);
+
+    var day = d.getDate();
+    var day_str = String(day);
+
+    // Ставим 0 перед значением дня, если нужно
+    if(day_str.length == 1){
+        day = "0" + day;
+    }
+
+    var month = d.getMonth()+1;
+    var month_str = String(month);
+
+    // Ставим 0 перед значением месяца, если нужно
+    if(month_str.length == 1){
+        month = "0" + month;
+    }
+
+    var year = d.getFullYear();
+    var previous_day = year + '-' + month + '-' + day;
+
+    elTrainingCalendar.value = previous_day;
+});
+
 
 // Форматируем дату
 function formatDate(date) {
