421c45dc2122cd1f48a00e89fe33a569b77747a8
SergeyMitkin <sergeymitkin@gmail.com>
2020-11-29 08:56:25 +0300

    Добавлена функции перелистывания календаря на следующий день

--- public/js/t_calendar.js
+++ public/js/t_calendar.js
@@ -57,8 +57,36 @@ elFlightButtonBack.addEventListener('click', event => {
     var previous_day = year + '-' + month + '-' + day;
 
     elTrainingCalendar.value = previous_day;
+    getItemsByDate();
 });
 
+// Получаем задачи на следующий день
+elFlightButtonForward.addEventListener('click', event => {
+    var d = new Date(getDate());
+    d.setDate(d.getDate() + 1);
+
+    var day = d.getDate();
+    var day_str = String(day);
+
+    // Ставим 0 перед значением дня, если нужно
+    if(day_str.length == 1){
+        day = "0" + day;
+    }
+
+    var month = d.getMonth()+1;
+    var month_str = String(month);
+
+    // Ставим 0 перед значением месяца, если нужно
+    if(month_str.length == 1){
+        month = "0" + month;
+    }
+
+    var year = d.getFullYear();
+    var next_day = year + '-' + month + '-' + day;
+
+    elTrainingCalendar.value = next_day;
+    getItemsByDate();
+});
 
 // Форматируем дату
 function formatDate(date) {
