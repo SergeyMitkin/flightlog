19993479a2c9ef40188c685bfafc290c2b293a99
SergeyMitkin <sergeymitkin@gmail.com>
2020-12-05 06:12:53 +0300

    Сделаны подписи для Сonfig.php

--- config/config.php
+++ config/config.php
@@ -1,25 +1,11 @@
 <?php
-define('SITE_ROOT', "../");
-define('VIEW_DIR', SITE_ROOT . 'views');
+// Константы директорий
+define('SITE_ROOT', "../"); // Корень сайта
+define('VIEW_DIR', SITE_ROOT . 'views'); // Папка шаблонами страниц
 
-// DB config
+// Контсанты для обращения к БД
 define('DB_DRIVER','mysql');
 define('DB_HOST','localhost');
 define('DB_NAME','flightlog');
 define('DB_USER','root');
 define('DB_PASS','');
-
-/*
-define('WWW_ROOT', SITE_ROOT . '/public');
-
-define('DATA_DIR', SITE_ROOT . 'data');
-define('LIB_DIR', SITE_ROOT . 'engine');
-define('TPL_DIR', SITE_ROOT . 'templates');
-
-define('SALT2', 'awOIHO@EN@Oine q2enq2kbkb');
-
-define('SITE_TITLE', 'Урок 8');
-
-require_once(LIB_DIR . '/lib_autoload.php');
-
-*/
